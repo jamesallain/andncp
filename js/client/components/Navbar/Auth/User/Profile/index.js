74fc807a79e7cbc23c4203ca7867f0d74431ed1b
nosh <james.allain@gmail.com>
2017-04-05 14:16:11 -0700

    first commit

---
+++ js/client/components/Navbar/Auth/User/Profile/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import React, {Component} from 'react';
+import Relay, {createContainer} from 'react-relay';
+import {Link} from 'react-router';
+
+class Profile extends Component {
+  render() {
+    return (
+      <li className = 'nav-item'>
+        <Link
+          className = 'nav-link'
+          to = {`/Profile/${this.props.user.profileId}`}
+        >
+          Profile
+        </Link>
+      </li>
+    );
+  }
+}
+
+export default createContainer(Profile, {
+  fragments: {
+    user() {
+      return Relay.QL`
+        fragment on User {
+          profileId
+        }
+      `;
+    }
+  }
+});
